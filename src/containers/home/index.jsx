d06b6b0534e0ef18819ddab1741bed1a7a5698a8
bhargav27b <bhargav@sketchbrahma.com>
2022-08-30 13:32:25 +0530

    project setup

---
+++ src/containers/home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './home.scss';
+import Banner from './sections/bannerSection';
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/containers/home/sections/bannerSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Banner() {
+  return (
+    <section className="bannerSection">
+      <p>
+        Solutions that make business better and life easier.
+      </p>
+    </section>
+  );
+}
